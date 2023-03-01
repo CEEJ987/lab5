01920370bf07f051e4b6702eaca86a64bea1a27d
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-03-01 11:56:20 -0600

    Update assignment

--- js/app.js
+++ js/app.js
@@ -9,8 +9,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    return [a + b, `The sum of ${a} and ${b} is ${a + b}.`
-    ]
+    return [a + b, `The sum of ${a} and ${b} is ${a + b}.`]
 
 }
 
@@ -29,11 +28,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    return [a * b, `The product of ${a} and ${b} is ${a * b}.`]
 }
 
+
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,11 +50,25 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  //  console.log(a + b + c, a * b * c,  ` ${a} and ${b} and ${c} sum ${a + b + c}`, `The product of ${a} and ${b} and ${c} is ${a * b * c}.`)
+  //  return [a + b + c, a * b * c,  ` ${a} and ${b} and ${c} sum ${a + b + c}`, `The product of ${a} and ${b} and ${c} is ${a * b * c}.`]
+    let x = sum(a,b)[0];
+   let  y = sum(x,c)[0];
+
+   let z = multiply(a,b)[0];
+   let w = multiply(z,c)[0];
+
+
+
+
+    return [y,w,`${a} and ${b} and ${c} sum to ${y}.`, `The product of ${a} and ${b} and ${c} is ${w}.`] 
+    
+
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
